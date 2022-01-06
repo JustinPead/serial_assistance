cbd93c28213ff64fcbd163651d78d2549ce15ee2
JustinPead87 <justinpead@gmail.com>
2022-01-06 18:05:49 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='serial_assistance',
+    version='0.01',
+    author='Justin Pead',
+    url='github.com',
+    py_modules=['serial_assistance'],
+    description='An script to find the available Serial ports and find a port by name',
+)
+
